2a4361a6f97ef4a2b6ad8322a70a231d119f46d6
ouyan1990 <138969261+ouyan1990@users.noreply.github.com>
2023-07-08 16:50:23 +0800

    Add files via upload
    Figure 2B Venn diagram

---
+++ venn.R
@@ -0,0 +1,35 @@
+
+install.packages("VennDiagram")
+
+
+library(VennDiagram)      
+
+setwd("E:\\bioinformation\\cuproptosis\\NAFLD1\\04 Venn")  
+geneList=list()
+
+#读取lasso回归的基因列表文件
+rt=read.table("diffGeneExp.txt" , header=F, sep="\t", check.names=F)
+geneNames=as.vector(rt[,1])              
+geneNames=geneNames[-1]
+geneNames=gsub("^ | $","",geneNames)    
+uniqGene=unique(geneNames)               
+geneList[["DEGs"]]=uniqGene             
+
+rt=read.table("gene.txt" , header=F, sep="\t", check.names=F)
+geneNames=as.vector(rt[,1])            
+geneNames=gsub("^ | $","",geneNames)     
+uniqGene=unique(geneNames)              
+geneList[["cuproptosis-related gene"]]=uniqGene          
+
+#Draw venn diagram 
+venn.plot=venn.diagram(geneList,filename=NULL,fill=c("#E41A1C", "#377EB8"),scaled=FALSE,cat.pos=c(-1,1),cat.col = c("cornflowerblue", "darkorchid1"),cat.cex = 1.2)
+pdf(file="venn.pdf", width=5, height=5)
+grid.draw(venn.plot)
+dev.off()
+
+#Intersection genes
+interGenes=Reduce(intersect,geneList)
+write.table(file="interGenes.txt", interGenes, sep="\t", quote=F, col.names=F, row.names=F)
+
+
+
