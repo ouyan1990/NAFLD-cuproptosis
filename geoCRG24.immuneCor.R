4ce90623d744cec680c54dc97ff5387911e4bab1
ouyan1990 <138969261+ouyan1990@users.noreply.github.com>
2023-07-08 17:34:05 +0800

    Add files via upload

---
+++ geoCRG24.immuneCor.R
@@ -0,0 +1,73 @@
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("reshape2")
+
+
+library(limma)
+library(reshape2)
+library(tidyverse)
+library(ggplot2)
+
+
+setwd("E:\\bioinformation\\cuproptosis\\NAFLD1\\23.immuneCor")  
+
+#Read the file and organize the input file
+rt=read.table("merge.txt" , header=T, sep="\t", check.names=F)
+rt=as.matrix(rt)
+rownames(rt)=rt[,1]
+exp=rt[,2:ncol(rt)]
+dimnames=list(rownames(exp),colnames(exp))
+data=matrix(as.numeric(as.matrix(exp)),nrow=nrow(exp),dimnames=dimnames)
+data=avereps(data)
+data=data[rowMeans(data)>0,]
+
+#Control sample removal
+group=gsub("(.*)\\_(.*)", "\\2", colnames(data))
+data=data[,group=="Treat",drop=F]
+
+#Read the gene list file and extract the expression of characteristic genes
+geneRT=read.table("interGenes.txt", header=F, sep="\t", check.names=F)
+data=data[as.vector(geneRT[,1]),]
+data=t(data)
+
+#Read the immune cell result file and organize the data
+immune=read.table("CIBERSORT-Results.txt", header=T, sep="\t", check.names=F, row.names=1)
+sameSample=intersect(row.names(data), row.names(immune))
+data=data[sameSample,,drop=F]
+immune=immune[sameSample,,drop=F]
+
+#Correlation analysis
+outTab=data.frame()
+for(cell in colnames(immune)){
+	if(sd(immune[,cell])==0){next}
+	for(gene in colnames(data)){
+		x=as.numeric(immune[,cell])
+		y=as.numeric(data[,gene])
+		corT=cor.test(x,y,method="spearman")
+		cor=corT$estimate
+		pvalue=corT$p.value
+		text=ifelse(pvalue<0.001,"***",ifelse(pvalue<0.01,"**",ifelse(pvalue<0.05,"*","")))
+		outTab=rbind(outTab,cbind(Gene=gene, Immune=cell, cor, text, pvalue))
+	}
+}
+
+#Draw a correlation heat map
+outTab$cor=as.numeric(outTab$cor)
+pdf(file="cor.pdf", width=7, height=4.5)
+ggplot(outTab, aes(Immune, Gene)) + 
+	geom_tile(aes(fill = cor), colour = "grey", size = 1)+
+	scale_fill_gradient2(low = "#02C9AF", mid = "white", high = "#E41A1C") + 
+	geom_text(aes(label=text),col ="black",size = 3) +
+	theme_minimal() +    #È¥µô±³¾°
+	theme(axis.title.x=element_blank(), axis.ticks.x=element_blank(), axis.title.y=element_blank(),
+	      axis.text.x = element_text(angle = 45, hjust = 1, size = 8, face = "bold"),   #xÖá×ÖÌå
+	      axis.text.y = element_text(size = 8, face = "bold")) +       #yÖá×ÖÌå
+	labs(fill =paste0("***  p<0.001","\n", "**  p<0.01","\n", " *  p<0.05","\n", "\n","Correlation")) +   #ÉèÖÃÍ¼Àý
+	scale_x_discrete(position = "bottom") +coord_flip()     #XÖáÃû³ÆÏÔÊ¾Î»ÖÃ
+dev.off()
+
+write.table(outTab,file = "corResult.txt", quote = F,sep = "\t")
+
+
+
+
