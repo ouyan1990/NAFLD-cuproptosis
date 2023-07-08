fe398699ff3148c92d09d1b821dd554688d59a68
ouyan1990 <138969261+ouyan1990@users.noreply.github.com>
2023-07-08 17:07:05 +0800

    Add files via upload

---
+++ 13.randomForest.R
@@ -0,0 +1,39 @@
+install.packages("randomForest")
+
+library(randomForest)
+set.seed(123)
+
+setwd("E:\\bioinformation\\cuproptosis\\NAFLD1\\13.RF")     
+data=read.table("diffGeneExp.txt"  , header=T, sep="\t", check.names=F, row.names=1)
+data=t(data)
+group=gsub("(.*)\\_(.*)", "\\2", row.names(data))
+
+rf=randomForest(as.factor(group)~., data=data, ntree=500)
+pdf(file="forest.pdf", width=6, height=6)
+plot(rf, main="Random forest", lwd=2)
+dev.off()
+
+optionTrees=which.min(rf$err.rate[,1])
+optionTrees
+rf2=randomForest(as.factor(group)~., data=data, ntree=optionTrees)
+
+importance=importance(x=rf2)
+
+
+pdf(file="geneImportance.pdf", width=6.2, height=5.8)
+varImpPlot(rf2, main="")
+dev.off()
+
+
+rfGenes=importance[order(importance[,"MeanDecreaseGini"], decreasing = TRUE),]
+rfGenes=names(rfGenes[rfGenes>2])      
+#rfGenes=names(rfGenes[1:5])          
+write.table(rfGenes, file="rfGenes.txt", sep="\t", quote=F, col.names=F, row.names=F)
+
+
+sigExp=t(data[,rfGenes])
+sigExpOut=rbind(ID=colnames(sigExp),sigExp)
+write.table(sigExpOut, file="rfGeneExp.txt", sep="\t", quote=F, col.names=F)
+
+
+
