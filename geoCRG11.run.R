9fd372cf77c83a5e26bd38ca5d1964770dd6b7db
ouyan1990 <138969261+ouyan1990@users.noreply.github.com>
2023-07-08 17:26:15 +0800

    Add files via upload

---
+++ geoCRG11.run.R
@@ -0,0 +1,23 @@
+
+
+install.packages('e1071')
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("preprocessCore")
+
+
+inputFile="merge.txt"     
+setwd("E:\\bioinformation\\cuproptosis\\NAFLD1\\22.CIBERSORT")     
+source("geoCRG11.CIBERSORT.R")      
+
+
+outTab=CIBERSORT("ref.txt", inputFile, perm=1000, QN=T)
+
+
+outTab=outTab[outTab[,"P-value"]<0.05,]
+outTab=as.matrix(outTab[,1:(ncol(outTab)-3)])
+outTab=rbind(id=colnames(outTab),outTab)
+write.table(outTab, file="CIBERSORT-Results.txt", sep="\t", quote=F, col.names=F)
+
+
